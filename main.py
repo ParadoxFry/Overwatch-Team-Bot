817d023219f3c52c765acc39200e28ee87c7b318
Ryan Fry <paradox999@yahoo.com>
2023-01-04 20:38:32 +0000

    Corrected issues with intents, and parameter names.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import discord
 
-clinet = discord.Client()
+intents = discord.Intents(messages=True)
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client)))
+  print('We have logged in as {0.user}'.format(client))
+  return
 
 @client.event
 async def on_message(message):
